41e76ee733242b4c6e150c50870d44e9832b73f1
Min Sohng <minsohng@gmail.com>
2019-11-05 13:37:51 -0800

    Fix metadata for args
    Fix metadata for args

--- src/utils/track.ts
+++ src/utils/track.ts
@@ -1,33 +1,47 @@
 import { sdk } from '@cto.ai/sdk'
 
-/*
- - user info
- - command 
- - error messages
- - time spent in ops
- - did user finish ops?
-*/
-
 interface metadata {
   error?: string
   totalTime?: string
+  awsHelp?: string
   isDone: boolean
 }
 
 export const track = async (metadata: metadata) => {
-  sdk.track(['track', 'aws'], {
+  const trackedData = {
     user: await sdk.user(),
     os: sdk.getHostOS(),
     isContainer: sdk.isContainer(),
     command: ['aws', ...sdk.yargs.argv._],
     ...metadata,
-  })
+  }
+  if (trackedData.command.length === 1) {
+    delete trackedData.command
+  }
+  if ('awsHelp' in metadata) {
+    trackedData['command'] = trackedData.awsHelp
+      .split(' ')
+      .filter(el => el !== '')
+    trackedData['powerUserArgs'] = ['aws', ...sdk.yargs.argv._]
+    delete trackedData.awsHelp
+  }
+  sdk.track(['track', 'aws'], trackedData)
 }
 
+let startTime
+let endTime
+
 export const startTimer = (): number => {
-  return process.hrtime()[0]
+  startTime = process.hrtime()[0]
+  return startTime
 }
 
 export const endTimer = (): number => {
-  return process.hrtime()[0]
+  endTime = process.hrtime()[0]
+  return endTime
+}
+
+export const getTotalTime = (): number => {
+  endTimer()
+  return endTime - startTime
 }
